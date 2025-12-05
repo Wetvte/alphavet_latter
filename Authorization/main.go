b18f3a6f2603a724824efbee9055e6c0af337e27
Wetvte <gleb.sigarew@mail.ru>
2025-12-05 17:30:58 +0300

    Добавлена папка для модуля авторизации

---
+++ Authorization/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Добро пожаловать в сервис авторизации!"))
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+
+	http.ListenAndServe(":8080", router)
+}
