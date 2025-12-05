ef820725305e3ede4d28e4aa943debc21a24e946
Wetvte <gleb.sigarew@mail.ru>
2025-12-05 17:48:51 +0300

    Добавлена обработка /login

--- Authorization/main.go
+++ Authorization/main.go
@@ -9,10 +9,14 @@ import (
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Добро пожаловать в сервис авторизации!"))
 }
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Добро пожаловать на страницу авторизации!"))
+}
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/login", HomeHandler)
 	
 	http.ListenAndServe(":8080", router)
 }
